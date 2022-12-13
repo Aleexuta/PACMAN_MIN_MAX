eff5002fea2e15c150b415b23153308efa61b2bf
Aleexuta <74063854+Aleexuta@users.noreply.github.com>
2022-12-13 17:07:34 +0200

    Main file

---
+++ main.py
@@ -0,0 +1,16 @@
+
+#0-gol
+#1-perete
+#2-bomboana
+#3-fantome
+#4-pacman
+matrix=[[],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        []]

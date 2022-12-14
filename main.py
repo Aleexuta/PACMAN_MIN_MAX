59c81a9577f89b469069ddf56a51a7cb2cb8be7b
liviubujdea <liviubujdea14@gmail.com>
2022-12-14 17:53:24 +0200

    commit 2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from enum import Enum#test
+import random
 #0-gol
 #1-perete
 #2-bomboana
@@ -43,16 +44,59 @@ def undo_move(CH,move,matrix): #dam undo la mutare. punem pe move CH( bomboana s
     if move[0] < 0 or move[0] >= N or move[1] < 0 or move[1] >= N:
         return False
  
-    board[move[0]][move[1]] = CH
+    matrix[move[0]][move[1]] = CH
     return True
 def print_Matrix(matrix): #afisam matricea in interfata sau normal
     return True
 
 def move_ghost(matrix,ghostPositions): #mutam random fantomele
-    return True
+    # facem random pe pozitii. 0 pt sus, 1 pt dreapta, 2 pt jos, 3 pt stanga
+    moved = False
+    i=ghostPositions[nr_gh].x
+    j=ghostPositions[nr_gh].y
+    while not moved:
+        pozitie_random = random.randint(0, 3)
+        if pozitie_random == 0:
+            if (i-1)>=0 and matrix[i-1][j] != PERETE:
+                ghostPositions[nr_gh]={'x':i-1,'y':j}
+                moved=True
+        if pozitie_random == 1:
+            if (j+1)<=N and matrix[i][j+1] != PERETE:
+                ghostPositions[nr_gh]={'x':i,'y':j+1}
+                moved = True
+        if pozitie_random == 0:
+            if (i+1)<=N and matrix[i+1][j] != PERETE:
+                ghostPositions[nr_gh]={'x':i+1,'y':j}
+                moved=True
+        if pozitie_random == 0:
+            if (j-1)>=0 and matrix[i][j-1] != PERETE:
+                ghostPositions[nr_gh]={'x':i,'y':j-1}
+                moved = True
 
 def check_win(matrix,ghostPositions, pacmanPosition): # verificam daca s-a terminat jocul, nr de bomboane==0, sau pacman este mancat 
-    return False
+    nr_bomboane=0
+    for i in range(N):
+        for j in range(N):
+            if(matrix[i][j]==BOMBOANA):
+                nr_bomboane+=1
+    
+    if nr_bomboane==0:
+        return (True,PACMAN)
+    
+    k=pacmanPosition.x
+    l=pacmanPosition.y
+    for nr_gh in range(ghost_number):
+        i=ghostPositions[nr_gh].x
+        j=ghostPositions[nr_gh].y
+
+        if(i==l and j==k):
+            return (True,FANTOMA)
+
+    return (False,-1)
+    
+    
+
+
 def possible_moves(CH,matrix): # ia toate posibilele mutari ale lui pacman(4) sau ale fantomelor(toate)
     #posibile adica sa nu aiba ziduri si chestii
     #pacman sa nu intre in fantoma
@@ -108,10 +152,11 @@ matrix[pacman_pos[0]][pacman_pos[1]]=0
 
 while not game_over:
     #o sa dam impresia ca pacman si fantoma se misca in acelasi timp pt ca le mutam pe ambele in aceasi iteratie
-
+    move_ghost(matrix, ghost_pos)
     #mutam fantoma
-    
+    move_pacman(matrix,pacman_pos,ghost_pos)
     #mutam pacman
     #verificam sa nu se fi terminat jocul
-
+    check_win(matrix,ghost_pos,pacman_pos)
     #afisam matricea
+    print_Matrix(matrix)

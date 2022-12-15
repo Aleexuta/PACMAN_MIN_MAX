389ce27b90ecc90d62b4e7f0f2f5533f73818545
Aleexuta <74063854+Aleexuta@users.noreply.github.com>
2022-12-15 10:09:37 +0200

    Complete evaluate function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from enum import Enum#test
+import numpy as np
 #0-gol
 #1-perete
 #2-bomboana
@@ -19,16 +20,18 @@ PACMAN=4
 #fantomele o sa se miste random
 #pacmanul isi va alege pozitia dupa algoritmul min-max alpha beta pruning
 
-matrix=[[2,2,2,1,2,2,2,1,1,2],
-        [2,1,2,2,2,1,2,2,1,2],
-        [2,1,1,1,1,1,1,2,1,2],
-        [2,1,2,2,2,1,2,2,2,2],
-        [2,2,2,1,1,1,2,1,2,1],
-        [2,1,2,2,1,2,2,1,2,2],
-        [2,1,1,2,1,2,1,1,1,2],
-        [2,2,2,2,2,2,2,1,2,2],
-        [1,1,1,1,1,2,1,1,2,1],
-        [2,2,2,2,2,2,2,2,2,2]]
+matrix=[[1,1,1,1,1,1,1,1,1,1,1,1]
+        [1,2,2,2,1,2,2,2,1,1,2,1],
+        [1,2,1,2,2,2,1,2,2,1,2,1],
+        [1,2,1,1,1,1,1,1,2,1,2,1],
+        [1,2,1,2,2,2,1,2,2,2,2,1],
+        [1,2,2,2,1,1,1,2,1,2,1,1],
+        [1,2,1,2,2,1,2,2,1,2,2,1],
+        [1,2,1,1,2,1,2,1,1,1,2,1],
+        [1,2,2,2,2,2,2,2,1,2,2,1],
+        [1,1,1,1,1,1,2,1,1,2,1,1],
+        [1,2,2,2,2,2,2,2,2,2,2,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1]]
 
 def do_move(move,matrix): # mananca bomboana de pe move
     #move e de forma (i,j)
@@ -53,29 +56,51 @@ def possible_moves(CH,matrix): # ia toate posibilele mutari ale lui pacman(4) sa
 def evaluate_move(matrix,pacmacPosition, ghostPositions, candyConsumed=0,depth=0,isMaximizingPlayer=True): #calculeaza costul mutarii ( arborele) si returneaza cea mai buna alegere 
     #functia de min/max
     #verificam sa nu fie la final ( game over /castig )
-
+    final,player=check_win(matrix,ghostPositions=ghostPositions,pacmanPosition=pacmacPosition)
+    if final==1:
+        if player==FANTOMA:
+            return 1000-depth
+        elif player==PACMAN:
+            return depth-1000
     #aici, la frunze e cel mai ciudatel
         # -1000 + depth pierde pacman
         # 1000 - depth + candyConsumed pana aici
 
-
+    evals=[]
     #if daca e isMax=True   ->pacman
+    if isMaximizingPlayer==True:
         #reapelam functia cu isMax=false
         #merge pe acelasi principiu ca la move.
         #luam toate posibilele mutari pacman ( lista cu pozitile de la pacman )
+        moves=possible_moves(PACMAN,matrix,ghost_pos,pacmacPosition)
+        for move in moves:
         #salvam ce se afla pe viitoarea pozitie
+            old_ch=matrix[move.x][move.y]
         #facem mutare, punem pe noua pozitia a pacmanului, ca e GOL
+            do_move(move,matrix)
         #evaluam noua posibila mutare, adaugam +1 la candyConsumed daca pe viitoarea este bomboana
+            if(old_ch==BOMBOANA):
+                 candy=candyConsumed+1
+            else:
+                 candy=candyConsumed
+            evals.append(evaluate_move(matrix,move,ghostPositions,candy,depth+1,False))
         #undo la mutare ( adaugam in matrice daca era bomboana sau nu)
+            undo_move(old_ch)
+        bestMove=max(evals)
         #calculam max din valorile
     #else    ->fantoma
+    else:
+
         #reapelam functia cu isMax=True
         #merge pe acelasi principiu ca la move.
         #luam toate posibilele mutari fantomelor
+        moves=possible_moves(FANTOMA,matrix,ghost_pos,pacmacPosition)
         #evaluam noua posibila mutare, matricea nu se schimba 
+        evals.append(evaluate_move(matrix,pacmacPosition,ghostPositions,candyConsumed,depth+1,True))
         #calculam min din valori
+        bestMove=min(evals)
     #returnam valoarea cea mai buna
-    return True
+    return bestMove
 def move_pacman(matrix,pacmanPosition, ghostPositions):
     
     #luam toate posibilele mutari pacman ( lista cu pozitile de la pacman )
@@ -84,16 +109,32 @@ def move_pacman(matrix,pacmanPosition, ghostPositions):
     #evaluam noua posibila mutare
     #candy consumed va fi 0  de la pozitia asta
     #undo la mutare ( adaugam in matrice daca era bomboana sau nu)
-
+    evals=[]
+    moves=possible_moves(PACMAN,matrix,ghost_pos,pacmanPosition)
+    for move in moves:
+    #salvam ce se afla pe viitoarea pozitie
+        old_ch=matrix[move.x][move.y]
+    #facem mutare, punem pe noua pozitia a pacmanului, ca e GOL
+        do_move(move,matrix)
+    #evaluam noua posibila mutare, adaugam +1 la candyConsumed daca pe viitoarea este bomboana
+        if(old_ch==BOMBOANA):
+                candy=1
+        else:
+                candy=0
+        evals.append(evaluate_move(matrix,move,ghostPositions,candy,0,False))
+    #undo la mutare ( adaugam in matrice daca era bomboana sau nu)
+        undo_move(old_ch)
+    best_move=moves[np.argmax(evals)]
+    do_move(best_move,matrix)
     #calculam cea mai buna valoare dintre valorile de la posibilele mutari
     #facem cea mai buna mutare pe tabla noastra (mancam bomboana si pacmanul)
     return False
 
 
 game_over=False
-pacman_pos=(0,0)
-ghost_pos=[(0,10),(10,0)]
-
+pacman_pos={'x':1,'y':1}
+ghost_pos=[{'x':1,'y':11},{'x':11,'y':1}]
+N=10
 matrix[pacman_pos[0]][pacman_pos[1]]=0
 
 while not game_over:
